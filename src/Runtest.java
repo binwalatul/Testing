51a163cf65a8855b25d67acdeebc092b54918653
Atul Binwal <binwal.atul@tftus.com>
2016-02-19 19:36:31 +0530

    Edoited commit

--- src/Runtest.java
+++ src/Runtest.java
@@ -1,4 +1,4 @@
-//import java.io.BufferedWriter;
+import java.io.BufferedWriter;
 //import java.io.FileOutputStream;
 //import java.io.OutputStreamWriter;
 //import java.io.PrintWriter;
@@ -21,25 +21,25 @@ public class Runtest {
 			System.out.println(driver.findElement(By.xpath("//section[@class='content-carousel-section']/h1")).getText());
 		}
 		
-		//section[@class='content-carousel-section']/h1
-		/*driver.get("https://www.qa.shutterstock.com/cs/");
+		section[@class='content-carousel-section']/h1
+		driver.get("https://www.qa.shutterstock.com/cs/");
 		driver.get("https://www.qa.shutterstock.com/it/");
 		driver.get("https://www.qa.shutterstock.com/en/");
 		driver.get("https://www.qa.shutterstock.com/zn/");
 		driver.get("https://www.qa.shutterstock.com/es/");
-		driver.get("https://www.qa.shutterstock.com/it/");*/
-//		driver.findElement(By.id("language-toggle")).click();
-//		WebElement drop = driver.findElement(By.xpath("//div[@id='shutterstock-header']/div/div/div[2]/div/ul"));
-//		System.out.println(drop.isEnabled());
-//		System.out.println(drop.isDisplayed());
-//		System.out.println(drop.getText());
-		//System.out.println(driver.findElement(By.xpath("//div[@id='content']/section[2]/h1")).getText());
-		//language-toggle
-		//Common.Searchphoto(driver);
-		//Common.Loginfromheader(driver);
-		//driver.getPageSource();
-		//System.out.println(driver.getPageSource());
-		//System.out.println(driver.getPageSource().contains(""));
+		driver.get("https://www.qa.shutterstock.com/it/"); 
+		driver.findElement(By.id("language-toggle")).click();
+		WebElement drop = driver.findElement(By.xpath("//div[@id='shutterstock-header']/div/div/div[2]/div/ul"));
+		System.out.println(drop.isEnabled());
+		System.out.println(drop.isDisplayed());
+		System.out.println(drop.getText());
+		System.out.println(driver.findElement(By.xpath("//div[@id='content']/section[2]/h1")).getText());
+		language-toggle
+		Common.Searchphoto(driver);
+		Common.Loginfromheader(driver);
+		driver.getPageSource();
+		System.out.println(driver.getPageSource());
+		System.out.println(driver.getPageSource().contains(""));
 		
 				
 		
